a22ee63f997871fd6317d16ac831ba0d65c9622e
patelabhay672 <patelabhay0302@gmail.com>
2025-05-26 21:29:10 +0530

    done

--- src/pages/Recipies.jsx
+++ src/pages/Recipies.jsx
@@ -6,7 +6,7 @@ const Recipies = () => {
 
   return (
     <div className="p-6 bg-gray-800 min-h-screen text-white">
-      <h1 className="text-3xl font-bold mb-8 text-center text-yellow-400">🍽️ My Recipes</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center text-yellow-400">🍽️Recipes</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {data.map((recipe) => (
